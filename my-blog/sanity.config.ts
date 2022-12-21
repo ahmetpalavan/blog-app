1912b8083a01e2050988dce728f240555f6630c4
ahmetpalavan <ahmetpalavann@gmail.com>
2022-12-21 23:18:47 +0300

    addition

--- my-blog/sanity.config.ts
+++ my-blog/sanity.config.ts
@@ -3,6 +3,8 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 import { myTheme } from './theme';
+import Logo from './components/Logo';
+import StudioNavbar from './components/StudioNavbar';
 
 const projectId=process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset=process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -18,6 +20,12 @@ export default defineConfig({
 
   schema: {
     types: schemaTypes,
+  },
+  studio: {
+    components: {
+      logo: Logo,
+      navbar: StudioNavbar
+    }
   },
   theme: myTheme 
 })
