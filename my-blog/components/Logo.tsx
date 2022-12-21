e7fb54106131af5f2f617bbe104e0e06f867d307
ahmetpalavan <ahmetpalavann@gmail.com>
2022-12-22 01:13:47 +0300

    added my github profile

--- my-blog/components/Logo.tsx
+++ my-blog/components/Logo.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Image from "next/image";
 
-function Logo() {
+function Logo(props: any) {
+  const { renderDefault, title } = props;
   return (
-    <div></div>
-  )
+    <div className="flex items-center space-x-2">
+      <Image
+        className="rounded-full object-cover" 
+        src="https://avatars.githubusercontent.com/u/105593137?s=400&u=97c5e25e12a8da3bbe89eaa08f453992cb2f1476&v=4"
+        alt="Ahmet Palavan"
+        width={50}
+        height={50}
+      />
+      <> {renderDefault(props)} </>
+    </div>
+  );
 }
 
-export default Logo
+export default Logo;
--- my-blog/components/StudioNavbar.tsx
+++ my-blog/components/StudioNavbar.tsx
@@ -9,6 +9,12 @@ function StudioNavbar(props: any) {
             <ArrowUturnLeftIcon className="h-6 w-6 text-[#F7AB0A] mr-2" />
             Go To Website
         </Link>
+        <div className="hidden md:flex p-5 rounded-lg justify-center border-2 border-[#F7AB0A]">
+            <h1 className="font-bold text-white">Want Coding challenges & Solutions sent to your inbox daily </h1>
+            <Link href="https://github.com/ahmetpalavan" className="font-bold ml-2 text-[#f7ab0a]">
+                https://github.com/ahmetpalavan
+            </Link>
+        </div>
         </div>
         <> {props.renderDefault(props)} </>
     </div>
